af7dd47ce19322aed828e771479c7ad127d1041f
RachelBernal <38430805+RachelBernal@users.noreply.github.com>
2018-05-18 18:21:40 -0500

    ejercicios tercer parcial

---
+++ cdr.c
@@ -0,0 +1,39 @@
+/*----------------------------------------------------------------------------*/
+/*Autor:Raquel Francisco Bernal                                               */
+/*E-mail:bernal-raquel@hotmail.com                                            */
+/*Fecha de creacion: 13/05/2018                                               */
+/*Fecha de modificacion:17/05/2018                                            */
+/*Descripcion: Programa que muestra una figura con *                    */
+/*----------------------------------------------------------------------------*/
+
+
+
+
+#include <stdio.h>
+#define TAMGIF 9
+
+void  dibujarFiguraContornoX(int tamanio);
+int main (void)
+{
+  dibujarFiguraContornoX(TAMGIF);
+  return 0;
+}
+
+void dibujarFiguraContornoX(int tamanio)
+{
+  for (int i=0; i<tamanio; i++)
+  {
+    for(int j=0; j<tamanio;j++)
+    {
+      if((i==0|| i ==j))
+      {
+      printf("*");
+      }else if(((tamanio-1))){
+      printf("*");
+      }else{
+	printf(" ");
+    }
+}
+    printf("\n");
+   }
+}
